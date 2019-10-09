a0bfcf230752393f352db5d062696370340d5f79
tudou0002 <1244491123@qq.com>
2019-10-09 09:45:59 -0400

    update immutable annotation

--- COMP303/src/lec10/Immutable.java
+++ COMP303/src/lec10/Immutable.java
@@ -7,7 +7,20 @@ import java.lang.annotation.Target;
 
 @Target(TYPE)
 @Immutable
+
+/**
+ * In a way, annotation is created for documentation purpose
+ * @author Yifei
+ *
+ */
 public @interface Immutable {
 	public String inspector() default "Yifei";
 	//@Immutable(inspector="Someone")
+	
+	/*
+	 * @Retention
+	 * Source: won't be detected e.g.@Override
+	 * Class: won't be detected
+	 * Runtime: can be detected e.g. @Test
+	 */
 }
